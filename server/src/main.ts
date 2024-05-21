c00cad9df209927eba1e1e5492429804b361c3bc
yg1110 <younggil9488@gmail.com>
2024-05-21 23:46:19 +0900

    swagger 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SnipSnip Swagger')
+    .setDescription('SnipSnip backend api description')
+    .setVersion('1.0.0')
+    .addTag('swagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    include: [AppModule],
+  });
+
+  const swaggerOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  SwaggerModule.setup('api', app, document, swaggerOptions);
+  await app.listen(8000);
 }
 bootstrap();
