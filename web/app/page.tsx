08fe3051e6768b457440a6092ed914084357ce6c
Hyeanwoo <wkdus7h@naver.com>
2024-06-16 20:15:07 +0900

    feat: draw login page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import { Button, Flex, Layout, Typography, Input, Space } from "antd";
+import {
+  layoutStyle,
+  headerStyle,
+  contentStyle,
+  flexStyle,
+  inputStyle,
+} from "@/app/ui/layoutStyle";
+
+const { Header, Content } = Layout;
+const { Title, Text } = Typography;
+
 export default function Home() {
   return (
-    <div>
-      <h1>Snip Snip</h1>
-    </div>
+    <Flex style={flexStyle}>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>
+          <Title>SnipSnip</Title>
+        </Header>
+        <Content style={contentStyle}>
+          <Space direction="vertical" align="center" size="middle">
+            <Space.Compact>
+              <div style={inputStyle}>
+                <Text>ID</Text>
+              </div>
+              <Input type="text" />
+            </Space.Compact>
+            <Space.Compact>
+              <div style={inputStyle}>
+                <Text>PASSWORD</Text>
+              </div>
+              <Input type="password" />
+            </Space.Compact>
+            <Space.Compact>
+              <Button type="text">login</Button>
+              <Button type="primary">register</Button>
+            </Space.Compact>
+          </Space>
+        </Content>
+      </Layout>
+    </Flex>
   );
 }
---
+++ web/app/ui/layoutStyle.ts
@@ -0,0 +1,34 @@
+export const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  height: 64,
+  color: "#0958d9",
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#ffffff",
+  marginTop: "220px",
+  width: "400px",
+};
+
+export const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  width: "400px",
+  marginTop: "50px",
+};
+
+export const layoutStyle: React.CSSProperties = {
+  borderRadius: 8,
+  overflow: "hidden",
+  alignItems: "center",
+  backgroundColor: "#ffffff",
+};
+
+export const flexStyle: React.CSSProperties = {
+  // height: "600px",
+  width: "1200px",
+  margin: "0 auto",
+};
+
+export const inputStyle: React.CSSProperties = {
+  width: "120px",
+  wordBreak: "normal",
+};
