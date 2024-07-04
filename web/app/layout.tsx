7f335d03d1ba3b09975c09ae62d96c2377150f35
Hyeanwoo <wkdus7h@naver.com>
2024-07-04 16:39:26 +0900

    chore: apply react-query

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,38 +1,27 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import {
-  HydrationBoundary,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { useState } from "react";
+import ReactQueryProvider from "@/app/lib/reactQueryProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "SnipSnip",
-//   description: "Control your all bookmarks easily!",
-// };
+export const metadata: Metadata = {
+  title: "SnipSnip",
+  description: "Control your all bookmarks easily!",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          {/* <HydrationBoundary> */}
+        <ReactQueryProvider>
           <AntdRegistry>{children}</AntdRegistry>
-          {/* </HydrationBoundary> */}
-        </QueryClientProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
