39ab0b6f865222627ef7565720b62a81bc29c3be
Hyeanwoo <wkdus7h@naver.com>
2024-07-05 23:41:11 +0900

    chore: remove test fetch file

--- web/app/lib/fetchTodos.ts
+++
@@ -1,8 +0,0 @@
-export const fetchTodos = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-
-  return response.json();
-};
