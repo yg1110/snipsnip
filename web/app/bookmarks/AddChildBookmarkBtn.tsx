99801599678af662848f8c5025cb5c79c1652a5c
Hyeanwoo <wkdus7h@naver.com>
2024-07-30 22:18:02 +0900

    feat: add adding child button group

---
+++ web/app/bookmarks/AddChildBookmarkBtn.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { Button, Form, Input, Modal, Select, message } from "antd";
+import { BookOutlined } from "@ant-design/icons";
+import { useChildFolders, useFolder } from "@/app/lib/data/query";
+import { useAddBookmark } from "@/app/lib/data/mutation";
+
+type AddChildBookmarkFormValue = {
+  title: string;
+  url: string;
+  folderId: number;
+};
+
+export default function AddChildBookmarkCBtn({
+  parentFolderId,
+}: {
+  parentFolderId: number;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { data: currentFolder } = useFolder(parentFolderId);
+  const { data: childFolders } = useChildFolders(parentFolderId);
+  const addBookmarkMutation = useAddBookmark();
+
+  const [form] = Form.useForm<AddChildBookmarkFormValue>();
+
+  useEffect(() => {
+    form.setFieldValue("folderId", currentFolder?.id);
+  }, [form, currentFolder]);
+
+  const folders = currentFolder && [currentFolder, ...(childFolders ?? [])];
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    form.resetFields();
+  };
+
+  const onSubmit = () => {
+    if (addBookmarkMutation.isPending) {
+      return;
+    }
+
+    const newBookmark = form.getFieldsValue();
+
+    addBookmarkMutation.mutate(
+      {
+        ...newBookmark,
+        order: 1,
+      },
+      {
+        onSuccess: () => {
+          message.success("새로운 즐겨찾기가 추가되었습니다.");
+          closeModal();
+        },
+      }
+    );
+  };
+
+  return (
+    <>
+      <Button size="small" icon={<BookOutlined />} onClick={openModal}>
+        북마크
+      </Button>
+      <Modal
+        title="새 북마크 추가하기"
+        open={isOpen}
+        onOk={onSubmit}
+        onCancel={closeModal}
+        confirmLoading={addBookmarkMutation.isPending}
+      >
+        <Form name="add-child-bookmark" form={form}>
+          <Form.Item label="제목" name="title">
+            <Input />
+          </Form.Item>
+          <Form.Item label="주소" name="url" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="폴더" name="folderId" rules={[{ required: true }]}>
+            <Select placeholder="Select a folder">
+              {folders?.map((folder) => (
+                <Select.Option key={folder.id} value={folder.id}>
+                  {folder.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+}
---
+++ web/app/bookmarks/AddChildFolderBtn.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { useAddFolder } from "@/app/lib/data/mutation";
+import { Button, Input, Modal, message } from "antd";
+import { FolderAddOutlined, FolderTwoTone } from "@ant-design/icons";
+
+const DEFAULT_FOLDER_NAME = "";
+const ENTER_KEYCODE = "Enter";
+
+export default function AddChildFolderBtn({
+  parentFolderId,
+}: {
+  parentFolderId: number;
+}) {
+  const [addFolderModalOpen, setAddFolderModalOpen] = useState(false);
+  const [folderName, setFolderName] = useState(DEFAULT_FOLDER_NAME);
+  const addFolderMutation = useAddFolder();
+
+  const openModal = () => {
+    setAddFolderModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setAddFolderModalOpen(false);
+    setFolderName("");
+  };
+
+  const addFolder = () => {
+    if (addFolderMutation.isPending) {
+      return;
+    }
+
+    addFolderMutation.mutate(
+      {
+        name: folderName,
+        order: 10,
+        parentFolderId,
+      },
+      {
+        onSuccess: () => {
+          message.success("새로운 폴더가 생성되었습니다.");
+          closeModal();
+        },
+      }
+    );
+  };
+
+  const submit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const key = e.key;
+
+    if (key !== ENTER_KEYCODE) {
+      return;
+    }
+
+    addFolder();
+  };
+
+  return (
+    <>
+      <Button size="small" icon={<FolderAddOutlined />} onClick={openModal}>
+        폴더
+      </Button>
+      <Modal
+        title="새 폴더 추가하기"
+        open={addFolderModalOpen}
+        onOk={addFolder}
+        onCancel={closeModal}
+        confirmLoading={addFolderMutation.isPending}
+      >
+        <Input
+          value={folderName}
+          onChange={(e) => setFolderName(e.target.value)}
+          placeholder="type your folder name"
+          prefix={<FolderTwoTone />}
+          onKeyUp={submit}
+        />
+      </Modal>
+    </>
+  );
+}
