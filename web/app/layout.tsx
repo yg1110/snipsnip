c812a65fde9fd8dd63835deb8cf7cd301fba0dfe
yg1110 <younggil9488@gmail.com>
2024-08-09 09:41:40 +0900

    메타태그 추가

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import ReactQueryProvider from '@/app/lib/reactQueryProvider';
@@ -7,8 +7,30 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'SnipSnip',
-  description: 'Control your all bookmarks easily!',
+  metadataBase: new URL('https://www.snipsnip.site'),
+  title: {
+    default: 'SnipSnip - 순간을 기록하는 북마크 플랫폼',
+    template: '%s | SnipSnip - 순간을 기록하는 북마크 플랫폼',
+  },
+  description:
+    '북마크를 빠르고 쉽게 필요한 부분만 따로 관리하고 싶다면 SnipSnip에서 시작하세요!',
+  keywords: ['북마크', 'SnipSnip', '순간', '기록'],
+  openGraph: {
+    type: 'website',
+    url: 'https://www.snipsnip.site',
+    title: 'SnipSnip - 순간을 기록하는 북마크 플랫폼',
+    description:
+      '북마크를 빠르고 쉽게 필요한 부분만 따로 관리하고 싶다면 SnipSnip에서 시작하세요!',
+    siteName: 'SnipSnip - 순간을 기록하는 북마크 플랫폼',
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
 };
 
 export default function RootLayout({
